859f9519a83e0ee4e2164f23163d54fcb144f8a7
Anatolii Volkodav <volkodav@ngti.nl>
2018-01-22 23:49:19 +0100

    implemented code from the lectures

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,8 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(2.0, 10.0, 0.4);
+  // Taken from the lecture Lesson 16, 11
+  pid.Init(0.2, 0.004, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
